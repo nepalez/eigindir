e7082333fb7eea78932cabe90511f2ddeb29ab78
Andrew Kozin <andrew.kozin@gmail.com>
2015-04-29 00:01:54 +0300

    [FEATURE] instance API
    == Addons
    
    * Eigindir#attributes
    * Eigindir#attributes=

--- lib/eigindir.rb
+++ lib/eigindir.rb
@@ -19,12 +19,45 @@ require_relative "eigindir/api"
 #
 # @author Andrew Kozin <Andrew.Kozin@gmail.com>
 #
+# @api public
 module Eigindir
+  using Patches
+
+  # Returns the hash of the object's attributes
+  #
+  # @return [Hash]
+  def attributes
+    __readers.zip(__readers.map(&method(:public_send))).to_h
+  end
+
+  # Assigns attributes from hash
+  #
+  # Unknown attributes are ignored
+  #
+  # @param [Hash] options
+  #
+  # @return [self] itself
+  def attributes=(options)
+    options
+      .normalize
+      .slice(*__writers)
+      .each { |key, value| public_send :"#{ key }=", value }
+  end
 
   # @!parse extend Eigindir::API
   # @private
   def self.included(klass)
     klass.extend API
+  end
+
+  private
+
+  def __readers
+    self.class.__send__ :__readers
+  end
+
+  def __writers
+    self.class.__send__ :__writers
   end
 
 end # module Eigindir
--- spec/tests/eigindir_spec.rb
+++ spec/tests/eigindir_spec.rb
@@ -2,6 +2,7 @@
 
 describe Eigindir do
 
+  let(:coercer) { ->(value) { value.to_s } }
   let(:klass) do
     Class.new do
       include Eigindir
@@ -23,5 +24,63 @@ describe Eigindir do
     end
 
   end # describe .included
+
+  describe "#attributes" do
+
+    subject do
+      instance.instance_eval "@foo = 1"
+      instance.attributes
+    end
+
+    it "returns coerced attributes" do
+      klass.attribute :foo, coerce: ->(val) { val.to_s }
+      expect(subject).to eq(foo: "1")
+    end
+
+    it "returns attributes with getter" do
+      klass.attribute_reader :foo, coerce: ->(val) { val.to_s }
+      expect(subject).to eq(foo: "1")
+    end
+
+    it "skips attributes without getter" do
+      klass.attribute_writer :foo, coerce: ->(val) { val.to_s }
+      expect(subject).to eq({})
+    end
+
+    it "skips non-attributes" do
+      klass.send :attr_accessor, :foo
+      expect(subject).to eq({})
+    end
+
+  end # describe #attributes
+
+  describe "#attributes=" do
+
+    subject do
+      instance.attributes = { "foo" => 1 }
+      instance.instance_eval "@foo"
+    end
+
+    it "sets values to coerced attributes" do
+      klass.attribute :foo, coerce: ->(val) { val.to_s }
+      expect(subject).to eq "1"
+    end
+
+    it "sets values to attributes with setter" do
+      klass.attribute_writer :foo, coerce: ->(val) { val.to_s }
+      expect(subject).to eq "1"
+    end
+
+    it "skips attributes without setter" do
+      klass.attribute_reader :foo, coerce: ->(val) { val.to_s }
+      expect(subject).to be_nil
+    end
+
+    it "skips non-attributes" do
+      klass.send :attr_accessor, :foo
+      expect(subject).to be_nil
+    end
+
+  end # describe #attributes=
 
 end # describe Eigindir
